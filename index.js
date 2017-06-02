6c973a6719a7b4353f3bceb9f2fe90ea8a98040c
devhulk <gerald.yerden@icloud.com>
2017-06-02 16:40:12 -0400

    added json in data folder, getNode

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 //Use Proxy and Reflect API as a handler for our action functions
 const Flow = require('./flow')
 const Node = require('./node')
+const fs = require('fs')
+const _ = require('lodash')
 
-  let task = new Node;
-  console.log(task.status)
-  Flow.statusComplete(task, 'complete')
-  console.log(task.status)
+let messages
+ fs.readFile('./data/messages.json','utf8',(err,data) => {
+  if (err) {console.error}
+  //console.log(data)
+  messages = data
+   getNodes()
+})
+
+let getNodes = () => {
+  //console.log(messages)
+  let nodes  = JSON.parse(messages)
+  nodes.forEach((action) => 
+    {
+      var newAction = _.assignIn(new Node, action)  
+      //console.log(newAction.name)
+    })
+
+}
+
+//  let task = new Node;
+//  console.log(task.status)
+//  Flow.statusComplete(task, 'complete')
+//  console.log(task.status)
+
+
